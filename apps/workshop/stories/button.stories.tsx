dcdfa9a214a68f2bd32c47ccf5b9a80212280212
linchengyi <zziteqaq@163.com>
2023-04-19 15:56:46 +0800

    feat: add workshop

---
+++ apps/workshop/stories/button.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from 'ui';
+
+export default {
+	component: Button,
+} as Meta<typeof Button>;
+
+export const Primary: StoryObj<typeof Button> = {};
